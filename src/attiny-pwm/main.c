b829b736425327924e43bde9316c740fd2ce5dd4
show <mamemomonga@gmail.com>
2019-10-10 17:23:43 +0900

    main.hへ移動

--- src/attiny-pwm/main.c
+++ src/attiny-pwm/main.c
@@ -1,30 +1,4 @@
-#include <avr/io.h>
-#include <util/delay.h>
-#include <avr/interrupt.h>
 #include "main.h"
-
-//      /RST |     | VCC
-//      PB3  |     | PB2 LED1
-// LED2 PB4  |     | PB1
-//      GND  |     | PB0
-
-#define LED2 ( 1<<PB4 )
-#define LED2_INIT DDRB  |=  LED2
-#define LED2_H    PORTB |=  LED2 
-#define LED2_L    PORTB &=~ LED2
-#define LED2_I    PORTB ^=  LED2
-
-#define LED1 ( 1<<PB2 )
-#define LED1_INIT DDRB  |=  LED1
-#define LED1_H    PORTB |=  LED1
-#define LED1_L    PORTB &=~ LED1
-#define LED1_I    PORTB ^=  LED1
-
-// PWMの周波数
-#define PWM_HZ 75
-
-// 1秒をカウントする
-uint8_t timer_ct0=0;
 
 // タイマ割込 TCNT0がOCR0Aになったら発動する
 // LED2はPWMで1秒ごとの点滅
@@ -43,6 +17,7 @@ int main(void) {
 	// 初期化
 	LED1_INIT;
 	LED2_INIT;
+	timer_ct0=0;
 
 	// 割り込み禁止
 	cli();
--- src/attiny-pwm/main.h
+++ src/attiny-pwm/main.h
@@ -1,5 +1,32 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#endif
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
 
+// 配線
+//       /RST |     | VCC
+//       PB3  |     | PB2 LED1
+//  LED2 PB4  |     | PB1
+//       GND  |     | PB0
+
+#define LED2 ( 1<<PB4 )
+#define LED2_INIT DDRB  |=  LED2
+#define LED2_H    PORTB |=  LED2 
+#define LED2_L    PORTB &=~ LED2
+#define LED2_I    PORTB ^=  LED2
+
+#define LED1 ( 1<<PB2 )
+#define LED1_INIT DDRB  |=  LED1
+#define LED1_H    PORTB |=  LED1
+#define LED1_L    PORTB &=~ LED1
+#define LED1_I    PORTB ^=  LED1
+
+// PWMの周波数
+#define PWM_HZ 75
+
+// 1秒カウンタ
+uint8_t timer_ct0;
+
+#endif
