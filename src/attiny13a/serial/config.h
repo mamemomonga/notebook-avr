acab602e24b6c01b6b69741d8b51fc813bf43c6f
show <mamemomonga@gmail.com>
2019-10-15 10:00:52 +0900

    修正

--- src/attiny13a/serial/config.h
+++ src/attiny13a/serial/config.h
@@ -1,7 +1,12 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
-#define BAUD 38400 
+// ボーレート 省略時は 38400
+// #define BAUD 38400 
+#define BAUD 115200
+
+// 送受信ポート、PBx
+// INT0割り込みを使う場合はPB1専用となる
 #define UART_Tx 1
 #define UART_Rx 1
 
--- src/attiny13a/serial/main.c
+++ src/attiny13a/serial/main.c
@@ -5,27 +5,31 @@
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <avr/eeprom.h>
 
 void serial_recieve(const char c) {
 	LED1_I;
-	cli(); serOutP("Recieve:"); serOutC(c); serOutP("\r\n"); sei();
+	cli(); serOutP("Recieve: ["); serOutC(c); serOutP("]\r\n"); sei();
 }
 
 int main(void) {
 	LED1_INIT;
-	LED1_H;
+	LED1_L;
 
 	// シリアル割り込み有効
 	serial_interrupt(serial_recieve);
 
 	// list_osccalを実行すると、文字化けとOSCCALリストが繰り返し出力される
-	// 正しく表示されたリストの中から中心値を選択すると
+	// 正しく表示されたリストの中から選択すると
 	// シリアル入出力に最適な動作周波数に調整できる。
 	// list_osccal();
 
 	// list_osccalからOSCCALの値を設定する
-	OSCCAL=0x60;
-	sei();
+	OSCCAL=0x62;
+	
+	_delay_ms(5000);
+	cli(); serOutP("Current OSCCAL = 0x"); serOutInt8Hex(OSCCAL); serOutP("\r\n"); sei();
+	LED1_L;
 
 	for(;;) {
 		LED1_I;
