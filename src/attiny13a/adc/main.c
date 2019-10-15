91d4d6a02607a6625fda75e63c40a1591badf785
show <mamemomonga@gmail.com>
2019-10-15 14:53:01 +0900

    変更

--- src/attiny13a/adc/main.c
+++ src/attiny13a/adc/main.c
@@ -6,36 +6,42 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-uint8_t adc1ReadByte(void) {
+void adc1Init(void) {
 	// Digital Input Disable Register 0
-	DIDR0 |= (1<<ADC1D); // ADC1(PB1)
+	DIDR0 |= (1<<ADC1D); // ADC1(PB2)
 
 	// ADC Multiplexer Selection Register
 	//   REFS0: Reference Selection Bit
 	//   ADLAR: ADC Left Adjust Result
 	//   MUX0, MUX1: Analog Channel Selection Bits
-	ADMUX = (1<<ADLAR)|(0<<MUX1)|(1<<MUX0); // Ref:Vcc, ADC1(PB1)
+	ADMUX = (1<<ADLAR)|(0<<MUX1)|(1<<MUX0); // Ref:Vcc, ADC1(PB2)
 
 	// ADC Control and Status Register A
 	ADCSRA = (1<<ADEN) // ADC Enable
-	 | (1<<ADSC) // ADC Start Conversion
 	 | (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0); // ADC Prescaler /128
+}
 
+uint8_t adc1ReadByte(void) {
+	// ADC Control and Status Register A
+	ADCSRA |= (1<<ADSC); // ADC Start Conversion
 	loop_until_bit_is_set(ADCSRA,ADIF); // ADIFになるまで待つ
-
 	return ADCH;
 }
 
 int main(void) {
-	LED1_INIT;
-	LED1_H;
 	
-	OSCCAL=0x60;
+	OSCCAL=0x62;
+	adc1Init();
+
+	uint8_t adcp;
 
 	for(;;) {
-		LED1_I;
-		serOutP("ADC1 Value:"); serOutInt8( adc1ReadByte() ); serOutP("\r\n");
-		_delay_ms(500);
+		uint8_t adc = adc1ReadByte();
+		if(adc != adcp) {
+			serOutP("ADC1 Value: "); serOutInt8(adc); serOutP("\r\n");
+			adcp = adc;
+			_delay_ms(10);
+		}
 	}
-
 }
+
